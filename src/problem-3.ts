f92431e49830116b796f00c360f1c4cc43fc89a3
JP Choyon Khan <kotjpckhan@gmail.com>
2024-11-07 15:05:38 +0600

    problem 3 are modified based on new requirement

--- src/problem-3.ts
+++ src/problem-3.ts
@@ -3,7 +3,6 @@
 
   // find word function
   function countWordOccurrences(sentence: string, word: string): number {
-    
     // make lowercase the sentence and word for case insensitive
     const lowSentence: string = sentence.toLocaleLowerCase();
     const lowWord: string = word.toLocaleLowerCase();
@@ -14,15 +13,15 @@
     // console.log(eachWord); //its show this [ 'typescript', 'is', 'great.', 'i', 'love', 'typescript!' ]
 
     // clean up the word with RegExp
-    const cleanUp: string[] = eachWord.map((cleanWord: string) =>
-      cleanWord.replace(/[^\w\s]|_/g, " ").replace(/\s+/g, " ")
-    );
+    // const cleanUp: string[] = eachWord.map((cleanWord: string) =>
+    //   cleanWord.replace(/[^\w\s]|_/g, " ").replace(/\s+/g, " ")
+    // );
     // console.log(cleanUp);
 
     // initial found the word
     let counter: number = 0;
-    for (let i = 0; i < cleanUp.length; i++) {
-      if (cleanUp[i] === lowWord) {
+    for (let i = 0; i < eachWord.length; i++) {
+      if (eachWord[i] === lowWord) {
         counter++;
       }
     }
@@ -30,7 +29,7 @@
   }
 
   const output: number = countWordOccurrences(
-    "TypeScript is great. I love TypeScript!",
+    "I love typescript",
     "typescript"
   );
 
