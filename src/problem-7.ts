a529582027b03a80e25b9ba2864583655300a5ab
JP Choyon Khan <kotjpckhan@gmail.com>
2024-11-07 15:26:51 +0600

    problem 7 are done successfully

--- src/problem-7.ts
+++ src/problem-7.ts
@@ -1,10 +1,19 @@
 {
   //
+  class Car {
+    constructor(
+      public name: string,
+      public model: string,
+      public year: number
+    ) {}
+    getCarAge() {
+      const age = 2024 - this.year;
+      console.log(age);
+    }
+  }
+  // Sample Input 1:
+  const car = new Car("Honda", "Civic", 2018);
+  car.getCarAge();
 
-
-
-
-
-
-  // 
+  //
 }
